1f399e793a15a82db02e3c3cc0f313c1c6d477f6
Sebastian <iseba22@icloud.com>
2016-12-13 15:18:41 +0100

    JS Stark I

---
+++ public_html/js/exercise1/solution/SebastianStark/ex3.js
@@ -0,0 +1,15 @@
+var x = prompt("Введите число");
+
+var z = 0;
+
+
+
+for( var i = 1; i <= x;i = i+1)
+
+    z+=i;
+
+document.write(z);
+
+
+
+
---
+++ public_html/js/exercise1/solution/SebastianStark/ex4.js
@@ -0,0 +1,7 @@
+var x = prompt("Введите число");
+var z,c;
+z = Math.floor(x / 2);
+c = x-z;
+document.write(z + "  парные" );
+
+document.write(c + "  непарные" );
