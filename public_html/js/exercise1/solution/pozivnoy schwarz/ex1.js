0c9f617078d5462276dc92416ba09a6d5e806a76
Mykola Yershov <my.yershov@gmail.com>
2017-01-17 13:05:43 +0200

    Kiril review

--- public_html/js/exercise1/solution/pozivnoy schwarz/ex1.js
+++ public_html/js/exercise1/solution/pozivnoy schwarz/ex1.js
@@ -1,6 +1,5 @@
-for (var i= 10; i <=20; i++) { 
-    if(i!=12){{
-            var s = Math.pow(i, 2);
-            document.write(s + " ");
+for (var i = 10; i <= 20; i++) { 
+    if (i !== 12) {
+        document.write(Math.pow(i, 2) + " is square of " + i + '<br />');
     }
-}}
+}
