7469190e7204c2985df9c6ba023edfc785a5aeed
Mykola Yershov <my.yershov@gmail.com>
2017-09-06 14:05:29 +0300

    Delete react starter pack

--- public_html/react/README.md
+++
@@ -1,9 +0,0 @@
-# Study project
-
-# Usage
-### Development
-```
-npm install
-npm start
-Open http://localhost:3000
-```
--- public_html/react/app/main.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/Home/Home.jsx'
-import ReactDOM from 'react-dom'
-import React from 'react'
-import './main.styl'
-
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-)
